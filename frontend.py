dca584c51bcd1757f633c43a1e52007ef2c0d4a7
Nishan Holla <hollanishan@gmail.com>
2023-12-31 11:49:12 +0530

    created pkl

--- frontend.py
+++ frontend.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.model_selection import GridSearchCV, ShuffleSplit, train_test_split, cross_val_score
 from sklearn.linear_model import Lasso, LinearRegression
 from sklearn.tree import DecisionTreeRegressor
+import pickle
 
 app = Flask(__name__)
 
@@ -31,128 +32,18 @@ def predict():
 def contact():
     return render_template('contact.html')
 
-def remove_bhk_outliers(df):
-    exclude_indices = np.array([])
-    for location, location_df in df.groupby('location'):
-        bhk_stats = {}
-        for bhk, bhk_df in location_df.groupby('bhk'):
-            bhk_stats[bhk] = {
-                'mean': np.mean(bhk_df.price_per_sqft),
-                'std': np.std(bhk_df.price_per_sqft),
-                'count': bhk_df.shape[0]
-            }
-        for bhk, bhk_df in location_df.groupby('bhk'):
-            stats = bhk_stats.get(bhk-1)
-            if stats and stats['count']>5:
-                exclude_indices = np.append(exclude_indices, bhk_df[bhk_df.price_per_sqft<(stats['mean'])].index.values)
-    return df.drop(exclude_indices,axis='index')
+import joblib
 
-def is_float(x):
-    try:
-        float(x)
-    except:
-        return False
-    return True
-    
-def convert_sqft_to_num(x):
-    tokens = x.split('-')
-    if len(tokens) == 2:
-        return (float(tokens[0])+float(tokens[1]))/2
-    try:
-        return float(x)
-    except:
-        return None
-    
-def remove_pps_outliers(df):
-    df_out = pd.DataFrame()
-    for key, subdf in df.groupby('location'):
-        m = np.mean(subdf.price_per_sqft)
-        st = np.std(subdf.price_per_sqft)
-        reduced_df = subdf[(subdf.price_per_sqft>(m-st)) & (subdf.price_per_sqft<=(m+st))]
-        df_out = pd.concat([df_out,reduced_df],ignore_index=True)
-    return df_out
-    
-def find_best_model_using_gridsearchcv(X,y):
-    algos = {
-        'linear_regression' : {
-            'model': LinearRegression(),
-              'params': {
-                'copy_X' : [True, False],
-                'fit_intercept' : [True, False],
-                'n_jobs' : [1,2,3],
-                'positive' : [True, False]
-            }
-        },
-        'lasso': {
-            'model': Lasso(),
-            'params': {
-                'alpha': [1,2],
-                'selection': ['random', 'cyclic']
-            }
-        },
-        'decision_tree': {
-            'model': DecisionTreeRegressor(),
-            'params': {
-                'criterion' : ['mse','friedman_mse'],
-                'splitter': ['best','random']
-            }
-        }
-    }
-    scores = []
-    cv = ShuffleSplit(n_splits=5, test_size=0.2, random_state=0)
-    for algo_name, config in algos.items():
-        gs =  GridSearchCV(config['model'], config['params'], cv=cv, return_train_score=False)
-        gs.fit(X,y)
-        scores.append({
-            'model': algo_name,
-            'best_score': gs.best_score_,
-            'best_params': gs.best_params_
-        })
-    return pd.DataFrame(scores,columns=['model','best_score','best_params'])
-
-def predict_price(location,sqft,bath,bhk):
-    df1 = pd.read_csv("bengaluru_house_prices.csv")
-    df2 = df1.drop(['area_type','society','balcony','availability'],axis='columns')
-    df3 = df2.dropna()
-    df3['bhk'] = df3['size'].apply(lambda x: int(x.split(' ')[0]))
-    df3.bhk.unique()
-    df3[df3['total_sqft'].apply(is_float)]
-    df4 = df3.copy()
-    df4.total_sqft = df4.total_sqft.apply(convert_sqft_to_num)
-    df4 = df4[df4.total_sqft.notnull()]
-    df5 = df4.copy()
-    df5['price_per_sqft'] = df5['price']*100000/df5['total_sqft']
-    df5.location = df5.location.apply(lambda x: x.strip())
-    location_stats = df5['location'].value_counts(ascending=False)
-    location_stats_less_than_10 = location_stats[location_stats<=10]
-    df5.location = df5.location.apply(lambda x: 'other' if x in location_stats_less_than_10 else x)
-    df6 = df5[~(df5.total_sqft/df5.bhk<300)]
-    df7 = remove_pps_outliers(df6)
-    df8 = remove_bhk_outliers(df7)
-    df9 = df8[df8.bath<df8.bhk+2]
-    df10 = df9.drop(['size','price_per_sqft'],axis='columns')
-    dummies = pd.get_dummies(df10.location)
-    df11 = pd.concat([df10,dummies.drop('other',axis='columns')],axis='columns')
-    df12 = df11.drop('location',axis='columns')
-    X = df12.drop(['price'],axis='columns')
-    y = df12.price
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=10)
-    lr_clf = LinearRegression()
-    lr_clf.fit(X_train,y_train)
-    lr_clf.score(X_test,y_test)
-    cv = ShuffleSplit(n_splits=5, test_size=0.2, random_state=0)
-    cross_val_score(LinearRegression(), X, y, cv=cv)
-    find_best_model_using_gridsearchcv(X,y)
-
+def predict_price(location, sqft, bath, bhk):
+    X = joblib.load('Eng_X.joblib')
+    lr_clf = joblib.load('linear_regression_model.joblib')
     loc_index = np.where(X.columns==location)[0][0]
-
     x = np.zeros(len(X.columns))
     x[0] = sqft
     x[1] = bath
     x[2] = bhk
     if loc_index >= 0:
         x[loc_index] = 1
-
     return (lr_clf.predict([x])[0] * 1.25)
 
 if __name__ == '__main__':
